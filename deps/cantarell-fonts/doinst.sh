37589a168ef8232a066943e648032a0b6ddd3bf6
w41l <walecha99@gmail.com>
2015-11-13 17:22:30 +0700

    Add wayland support for gdm, gnome-session, gnome-settings-daemon, mutter, clutter, and cogl. Update budgie-desktop. Update cantarell-fonts doinst.sh.

--- deps/cantarell-fonts/doinst.sh
+++ deps/cantarell-fonts/doinst.sh
@@ -1,4 +1,12 @@
-
-if [ -d usr/share/fonts/cantarell ]; then
-    /usr/bin/fc-cache -f -s >/dev/null 2>&1
+# There's no need to chroot and do this during initial
+# install, since there is a post-install script that
+# does the same thing, saving time.
+# Update X font indexes and the font cache:
+if [ -x /usr/bin/mkfontdir ]; then
+  /usr/bin/mkfontscale /usr/share/fonts/TTF
+  /usr/bin/mkfontdir /usr/share/fonts/TTF
 fi
+if [ -x /usr/bin/fc-cache ]; then
+  /usr/bin/fc-cache /usr/share/fonts/TTF
+fi
+
