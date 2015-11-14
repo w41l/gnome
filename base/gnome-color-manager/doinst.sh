41d9bb9120f6c6fc169492c997c2279d14d54dde
w41l <walecha99@gmail.com>
2015-11-14 19:20:42 +0700

    Reordering Packages for Sbo, deps, and base

--- base/gnome-color-manager/doinst.sh
+++ base/gnome-color-manager/doinst.sh
@@ -1,13 +1,13 @@
 
-if [ -x /usr/bin/rarian-sk-update ]; then
+if [ -d usr/share/help ]; then
   /usr/bin/rarian-sk-update 1> /dev/null 2> /dev/null
 fi
 
-if [ -x /usr/bin/update-desktop-database ]; then
+if [ -d usr/share/applications ]; then
   /usr/bin/update-desktop-database -q /usr/share/applications >/dev/null 2>&1
 fi
 
-if [ -x /usr/bin/update-mime-database ]; then
+if [ -d usr/share/mime ]; then
   /usr/bin/update-mime-database /usr/share/mime >/dev/null 2>&1
 fi
 
@@ -17,7 +17,6 @@ if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
   fi
 fi
 
-if [ -x /usr/bin/glib-compile-schemas -a -d /usr/share/glib-2.0/schemas ]; then
+if [ -d usr/share/glib-2.0/schemas ]; then
   /usr/bin/glib-compile-schemas /usr/share/glib-2.0/schemas >/dev/null 2>&1
 fi
-
--- base/nautilus/doinst.sh
+++ base/nautilus/doinst.sh
@@ -1,23 +1,22 @@
 
-if [ -x /usr/bin/rarian-sk-update ]; then
+if [ -d usr/share/help ]; then
   /usr/bin/rarian-sk-update 1> /dev/null 2> /dev/null
 fi
 
 if [ -d usr/share/applications ]; then
-  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+  /usr/bin/update-desktop-database -q /usr/share/applications >/dev/null 2>&1
 fi
 
 if [ -d usr/share/mime ]; then
-  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+  /usr/bin/update-mime-database /usr/share/mime >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache /usr/share/icons/hicolor >/dev/null 2>&1
+  fi
 fi
 
 if [ -d usr/share/glib-2.0/schemas ]; then
-  /usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas >/dev/null 2>&1
+  /usr/bin/glib-compile-schemas /usr/share/glib-2.0/schemas >/dev/null 2>&1
 fi
-
-if [ -f /usr/share/applications/Thunar-folder-handler.desktop ]; then
-  echo "Disabling Thunar GNOME folder handling."
-  mv -f /usr/share/applications/Thunar-folder-handler.desktop \
-   /usr/share/applications/Thunar-folder-handler.desktop.disabled
-fi;
-
