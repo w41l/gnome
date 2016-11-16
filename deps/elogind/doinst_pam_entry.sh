d9523a5998da3a51e0611c89a5aff4a757b3ade4
Widya Walesa <walecha99@gmail.com>
2016-11-17 02:19:12 +0700

    Now GNOME3 can run with elogind so I'm removing all consolekit based initialization
    Signed-off-by: Widya Walesa <walecha99@gmail.com>

--- deps/elogind/doinst_pam_entry.sh
+++ deps/elogind/doinst_pam_entry.sh
@@ -1,7 +1,7 @@
 
 if [ -r /etc/pam.d/system-session ]; then
-  echo "WARNING: Spamming /etc/pam.d/system-session with pam_systemd.so"
-  if ! grep "pam_systemd" /etc/pam.d/system-session >/dev/null; then
-    echo "session   optional    pam_systemd.so" >> /etc/pam.d/system-session
+  echo "WARNING: Spamming /etc/pam.d/system-session with pam_elogind.so"
+  if ! grep "pam_elogind" /etc/pam.d/system-session >/dev/null; then
+    echo "session   optional    pam_elogind.so" >> /etc/pam.d/system-session
   fi
 fi
