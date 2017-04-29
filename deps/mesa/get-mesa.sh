2f56563fe3fafc2cd3c9e19324d52ab18510c384
Widya Walesa <walecha99@gmail.com>
2017-04-29 20:38:51 +0700

    Cleaning up old packages in deps:
    * pam_unix2: Removed.
    * gst-plugins-bad: Removed.
    * libinput: Use Slackware-current package.
    * wayland: Removed.
    * wayland-protocols: Removed.
    * libomxil-bellagio: Use Slackware-current package (no wayland).
    * mesa: Removed.
    
    Signed-off-by: Widya Walesa <walecha99@gmail.com>

--- deps/mesa/get-mesa.sh
+++ deps/mesa/get-mesa.sh
@@ -11,10 +11,10 @@ HEADISAT="$(cat mesa/.git/packed-refs | grep "refs/remotes/origin/${BRANCH}$" | 
 # Cleanup.  We're not packing up the whole git repo.
 ( cd mesa && find . -type d -name ".git*" -exec rm -rf {} \; 2> /dev/null )
 DATE=$(date +%Y%m%d)
-mv mesa Mesa-${BRANCH}_${HEADISAT}
-tar cf MesaLib-${BRANCH}_${HEADISAT}.tar Mesa-${BRANCH}_${HEADISAT}
-xz -9 MesaLib-${BRANCH}_${HEADISAT}.tar
-rm -rf MesaLib-${BRANCH}_${HEADISAT}
+mv mesa mesa-${BRANCH}_${HEADISAT}
+tar cf mesa-${BRANCH}_${HEADISAT}.tar mesa-${BRANCH}_${HEADISAT}
+xz -9 mesa-${BRANCH}_${HEADISAT}.tar
+rm -rf mesa-${BRANCH}_${HEADISAT}
 echo
-echo "Mesa branch $BRANCH with HEAD at $HEADISAT packaged as MesaLib-${BRANCH}_${HEADISAT}.tar.xz"
+echo "Mesa branch $BRANCH with HEAD at $HEADISAT packaged as mesa-${BRANCH}_${HEADISAT}.tar.xz"
 echo
